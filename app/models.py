d4ce908cf6b95e26b6c1cd2e39ac3971a5f8e2b3
arleiverga <36283207+arleiverga@users.noreply.github.com>
2018-04-27 20:11:29 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 # Create your models here.
 class Curso(models.Model):
-    nome = models.CharField(max_length=200)
-    periodo = models.CharField(max_length=50)
-    instituicao = models.CharField(max_length=200)
+    nome = models.CharField(max_length=300)
+    periodo = models.CharField(max_length=40)
+    instituicao = models.CharField(max_length=300)
 
